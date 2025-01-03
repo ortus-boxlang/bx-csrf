fd7e859b46610cab4b963d45aee1ed3d9de56bff
Jon Clausen <jclausen@ortussolutions.com>
2025-01-03 13:54:12 -0500

    remove empty stack execution

--- src/test/java/ortus/boxlang/modules/csrf/IntegrationTest.java
+++ src/test/java/ortus/boxlang/modules/csrf/IntegrationTest.java
@@ -9,22 +9,22 @@ import ortus.boxlang.runtime.scopes.Key;
 
 public class IntegrationTest extends BaseIntegrationTest {
 
-	@DisplayName( "Test the module loads in BoxLang" )
+	@DisplayName("Test the module loads in BoxLang")
 	@Test
 	public void testModuleLoads() {
 		// Verify things got registered
-		assertThat( moduleService.getRegistry().containsKey( moduleName ) ).isTrue();
-		assertThat( runtime.getSchedulerService().hasScheduler( Key.of( "CSRFTokenReaper@csrf" ) ) ).isTrue();
-		assertThat( runtime.getFunctionService().hasGlobalFunction( "CSRFGenerateToken" ) ).isTrue();
-		assertThat( runtime.getFunctionService().hasGlobalFunction( "CSRFVerifyToken" ) ).isTrue();
+		assertThat(moduleService.getRegistry().containsKey(moduleName)).isTrue();
+		assertThat(runtime.getSchedulerService().hasScheduler(Key.of("CSRFTokenReaper@csrf"))).isTrue();
+		assertThat(runtime.getFunctionService().hasGlobalFunction("CSRFGenerateToken")).isTrue();
+		assertThat(runtime.getFunctionService().hasGlobalFunction("CSRFVerifyToken")).isTrue();
 
 		// @formatter:off
-		runtime.executeSource(
-		    """
-			// Testing code here
-			""",
-		    context
-		);
+		// runtime.executeSource(
+		//     """
+		// 	// Testing code here
+		// 	""",
+		//     context
+		// );
 		// @formatter:on
 	}
 }
