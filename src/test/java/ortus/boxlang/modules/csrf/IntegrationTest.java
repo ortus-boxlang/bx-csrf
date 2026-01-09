91255eb440ddfbe3d5a32b5cb724572a4d2beccf
Jon Clausen <jclausen@ortussolutions.com>
2026-01-09 15:00:33 -0500

    BLMODULES-121 Resolve - Update to default to use current application session cache and change storage to single entry per token

--- src/test/java/ortus/boxlang/modules/csrf/IntegrationTest.java
+++ src/test/java/ortus/boxlang/modules/csrf/IntegrationTest.java
@@ -5,8 +5,6 @@ import static com.google.common.truth.Truth.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import ortus.boxlang.runtime.scopes.Key;
-
 public class IntegrationTest extends BaseIntegrationTest {
 
 	@DisplayName( "Test the module loads in BoxLang" )
@@ -14,7 +12,6 @@ public class IntegrationTest extends BaseIntegrationTest {
 	public void testModuleLoads() {
 		// Verify things got registered
 		assertThat( moduleService.getRegistry().containsKey( moduleName ) ).isTrue();
-		assertThat( runtime.getSchedulerService().hasScheduler( Key.of( "CSRFTokenReaper@csrf" ) ) ).isTrue();
 		assertThat( runtime.getFunctionService().hasGlobalFunction( "CSRFGenerateToken" ) ).isTrue();
 		assertThat( runtime.getFunctionService().hasGlobalFunction( "CSRFVerifyToken" ) ).isTrue();
 
